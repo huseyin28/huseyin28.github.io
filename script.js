fc223d71cac8bc839c45370fc7bbb4bc3d5e1252
Hüseyin Yılmaz <huseyin2219@gmail.com>
2022-04-06 06:32:27 +0300

    metraj hesabı

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-alert("hoşgeldin");
+function Hesapla() {
+    let halat = Number($('#dia').val())
+    let makara_capi = cm2mm(Number($('#makara_cap').val()))
+    let gobek_capi = cm2mm(makara_capi - Number($('#derinlik').val()))
+    let h = cm2mm(Number($('#aralik').val()))
+
+    let brutHacim = getVolume(makara_capi, h)
+    let gobekHacim = getVolume(gobek_capi, h)
+    let netHacim = brutHacim - gobekHacim
+
+    let halatHacim = Math.pow(halat,2) * cm2mm(100)
+
+    alert(Math.floor(netHacim / halatHacim))
+}
+
+function getVolume(r, h) {
+    return Math.PI * Math.pow(r,2) * h
+}
+
+function cm2mm(val){
+    return val * 10
+}
