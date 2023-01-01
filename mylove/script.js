383675b62a3ee7402bb6ec10bcbde9e7108b2baa
Hüseyin Yılmaz <huseyin2219@gmail.com>
2023-01-01 23:37:33 +0300

    sistem tamam gibi

--- mylove/script.js
+++ mylove/script.js
@@ -14,6 +14,41 @@ let meetings = [
         date : "3. Buluşma <br> 06 Kasım 2021",
         dtObj : new Date(2021,10,6),
         fileName : "20211106.jpg"
+    },
+    {
+        date : "4. Buluşma <br> 25 Aralık 2021",
+        dtObj : new Date(2021,11,25),
+        fileName : "20211225.jpg"
+    },
+    {
+        date : "5. Buluşma <br> 05 Mayıs 2022",
+        dtObj : new Date(2022,4,5),
+        fileName : "20220505.jpg"
+    },
+    {
+        date : "6. Buluşma <br> 21 Mayıs 2022",
+        dtObj : new Date(2022,4,21),
+        fileName : "20220521.jpg"
+    },
+    {
+        date : "7. Buluşma <br> 18 Haziran 2022",
+        dtObj : new Date(2022,5,18),
+        fileName : "20220618.jpg"
+    },
+    {
+        date : "8. Buluşma <br> 13 Ağustos 2022",
+        dtObj : new Date(2022,7,13),
+        fileName : "20220813.jpg"
+    },
+    {
+        date : "9. Buluşma <br> 09 Ekim 2022",
+        dtObj : new Date(2022,9,9),
+        fileName : "20221009.jpg"
+    },
+    {
+        date : "9. Buluşma <br> 10 Aralık 2022",
+        dtObj : new Date(2022,11,10),
+        fileName : "20221210.jpg"
     }
 ]
 
