5d7a06e138cc713218858feaaa0b3c9e0afb8f96
Hüseyin Yılmaz <huseyin2219@gmail.com>
2023-01-17 22:41:59 +0300

    buluşmalar demo aktif

--- mylove/script.js
+++ mylove/script.js
@@ -1,4 +1,32 @@
 $(document).ready(function () {
+    neKadar();
+    getBulusmalar();
+
+    
+
+})
+
+function getBulusmalar(){
+    $.ajax({
+        url : "./meetings.json",
+        success : response => {
+            $('#bulusmalar').html('')
+            response.forEach(element => {
+                $('#bulusmalar').append(`<div class="col-lg-4 col-sm-6">
+                    <a class="portfolio-box" href="assets/img/portfolio/fullsize/1.jpg" title="Project Name">
+                        <img class="img-fluid" src="images/${element.fileName}" alt="..." />
+                        <div class="portfolio-box-caption">
+                            <div class="project-category text-white-50">Category</div>
+                            <div class="project-name">Project Name</div>
+                        </div>
+                    </a>
+                </div>`)
+            });
+        }
+    })
+}
+
+function neKadar() {
     let start = new Date(2020, 5, 3);
     let now = new Date(Date.now());
 
@@ -16,4 +44,4 @@ $(document).ready(function () {
         dday += 31;
     }
     $('#neKadar').html(`${dyear} yıl, ${dmonth} ay, ${dday} gün`);
-})
+}
